cbc5e1da7317678b212c9a87e194ec3c27b9f833
08Miguel24 <MichaelAveuc571@gmail.com>
2022-11-11 21:05:12 +0800

    committing new files

--- data_etl_scripts/visualizer.py
+++ data_etl_scripts/visualizer.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('Major Philippine Labor Law Resources page details data.csv')
+df = pd.read_csv('Major Philippine Labor Law Resources page details data.csv', index_col=0)
 
 top = df.head()
 
-top
+df
