db449b8557cec51269a5e42feade883a833106e6
08Miguel24 <MichaelAveuc571@gmail.com>
2022-11-18 21:26:30 +0800

    updated notes

--- raw data cleaning/test.py
+++ raw data cleaning/test.py
@@ -2,4 +2,7 @@ import pandas as pd
 
 # data = pd.read_csv('../data_etl_scripts/Major Philippine Labor Law Resources page details data.csv', index_col=0)
 
-ENTITY_TYPES = ['person', 'company', 'enterprise', 'business', 'geographic region', 'geographic entity', 'organization']
+ENTITY_TYPES = ['article', 'section', 'chapter', 'person', 'company', 'enterprise', 'business', 'geographic region', 'geographic entity', 'organization']
+
+# this is for jurisprudence
+# who was doing who, date where it was committed, name of the person, 
