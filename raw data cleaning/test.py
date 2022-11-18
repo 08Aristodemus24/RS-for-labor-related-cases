e829b6764819363396fcf8d3ae05527744311421
08Miguel24 <MichaelAveuc571@gmail.com>
2022-11-18 18:58:58 +0800

    added new directory for cleaning raw text

---
+++ raw data cleaning/test.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# data = pd.read_csv('../data_etl_scripts/Major Philippine Labor Law Resources page details data.csv', index_col=0)
+
+ENTITY_TYPES = ['person', 'company', 'enterprise', 'business', 'geographic region', 'geographic entity', 'organization']
