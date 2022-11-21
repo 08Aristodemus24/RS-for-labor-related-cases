4b23abb82ff8d1a0a95c74dc113751ed95cc8706
08Miguel24 <MichaelAveuc571@gmail.com>
2022-11-21 20:36:51 +0800

    cleaned raw data files, added script for neo4j querying

--- raw data cleaning/test.py
+++ raw data cleaning/test.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import spacy
+import neuralcoref
+import nltk
 
-# data = pd.read_csv('../data_etl_scripts/Major Philippine Labor Law Resources page details data.csv', index_col=0)
-
-ENTITY_TYPES = ['article', 'section', 'chapter', 'person', 'company', 'enterprise', 'business', 'geographic region', 'geographic entity', 'organization']
+df = pd.read_csv('./Major Philippine Labor Law Resources.csv', index_col=0)
 
 # this is for jurisprudence
 # who was doing who, date where it was committed, name of the person, 
+ENTITY_TYPES = ['article', 'section', 'chapter', 'person', 'company', 'enterprise', 'business', 'geographic region', 'geographic entity', 'organization']
+
+
+
