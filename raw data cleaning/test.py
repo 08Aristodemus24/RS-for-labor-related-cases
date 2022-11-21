ea97e15db9a85e923115e99f8825399b9c5b2aba
08Miguel24 <MichaelAveuc571@gmail.com>
2022-11-21 20:45:52 +0800

    cleaned raw data files, added script for neo4j querying

--- raw data cleaning/test.py
+++ raw data cleaning/test.py
@@ -7,7 +7,14 @@ df = pd.read_csv('./Major Philippine Labor Law Resources.csv', index_col=0)
 
 # this is for jurisprudence
 # who was doing who, date where it was committed, name of the person, 
-ENTITY_TYPES = ['article', 'section', 'chapter', 'person', 'company', 'enterprise', 'business', 'geographic region', 'geographic entity', 'organization']
-
+ENTITY_TYPES = [
+    'article', 'section', 'chapter', 
+    'person', 'company', 'enterprise', 
+    'business', 'geographic region', 'geographic entity', 
+    'organization'
+]
+# load SpaCy then add instance to pipeline
+nlp = spacy.load('en-core-web-sm')
+neuralcoref.add_to_pipe(nlp)
 
 
