01219cd038cfb3a8cbb6db6f946e0337338eab0e
08Miguel24 <MichaelAveuc571@gmail.com>
2022-11-30 21:44:28 +0800

    modified .gitignore file

--- test.js
+++ test.js
@@ -1,1 +1,38 @@
-return document.querySelector(`input[type="submit"]#MainContent_btnOCRConvert`).getAttribute("disabled") !== null
+const fs = require('fs');
+
+fs.readFile("./commit-data.json", "utf8", (error, json_string) => {
+    if(error){
+        console.log("file reading failed");
+        return;
+    }
+    
+    try{
+        console.log(json_string)
+        
+        // define output string
+        let output = "";
+
+        // returns a list of all commits
+        const commit_data = JSON.parse(json_string);
+
+        // parse each commit and extract message
+        for(let commit of commit_data){
+            output += `${commit["commit"]["message"]}\n`;
+        }
+        console.log(output);
+        
+        fs.writeFile("./parsed-commit-data.txt", output, (error) => {
+            if(error){
+                console.log("error occured while writing file");
+            }else{
+                console.log("writing successful");
+            }
+        })
+
+    }catch(error){
+        console.log(`error ${error} occurred`);
+    }
+});
+        
+
+          
